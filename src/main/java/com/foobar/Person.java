2e2a238d1618f3d1986ba8bc2cef2845fe43332f
Krishnan Mani <km@krishnanm.com>
2021-12-07 03:22:22 +0000

    simple class without builder

---
+++ src/main/java/com/foobar/Person.java
@@ -0,0 +1,11 @@
+package com.krishnanm.foobar;
+
+public class Person {
+
+	private final String name;
+
+	public Person(String name) { this.name = name; }
+
+	public String getName() { return name; }
+
+}
---
+++ src/test/java/com/foobar/PersonTest.java
@@ -0,0 +1,15 @@
+package com.krishnanm.foobar;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersonTest {
+
+	@Test
+	void itIsNamed() {
+          Person person = new Person("Adam");
+		assertEquals("Adam", person.getName());
+	}
+
+
+}
